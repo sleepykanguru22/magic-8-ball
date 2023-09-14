1675bda134b4d3778c88c2fed0b176ab88b6d813
Jasmin Carter <70289696+sleepykanguru22@users.noreply.github.com>
2023-09-13 23:59:40 -0700

    updates

--- magic8ball-23/magic8ball/src/App.jsx
+++ magic8ball-23/magic8ball/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Scene from "./components/Scene";
 import Form from "./components/Form";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <div>
         <Scene />
         <Form />
+        <Footer />
       </div>
     </>
   );
